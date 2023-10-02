04edc182b80fd00a2b7a8a1cb332448401d93a24
WebDevAsimAli <146151821+WebDevAsimAli@users.noreply.github.com>
2023-10-02 15:08:45 +0000

    Conversions

---
+++ .devcontainer/01_basics/03_conversionsOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+let myName = "33abvnb"
+
+//console.log(typeof score);
+//console.log(typeof myName);
+
+let valueInNumber = Number(myName)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abcvd" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Ali"
+let BoleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BoleanisLoggedIn);
+// 1 => true; 0 => false
+//  " " => False ; "Ali" => True
+
+let someNum = 45
+let stringNum  = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);

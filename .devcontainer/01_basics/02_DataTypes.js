427794d43a060114d017b28b85c48dded7df1e17
WebDevAsimAli <146151821+WebDevAsimAli@users.noreply.github.com>
2023-10-02 14:33:03 +0000

    Cover the concept of Data Types

---
+++ .devcontainer/01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+// Treat all Js code as a newer version
+// "use strict";
+// alert(3+3) 
+ // we are using node js ; not brwoser
+// Code readability should be high 
+let name = "Asim Ali"
+let Age = 34
+let isLoggedIn = true
+
+// Primitive Data Types
+// Number => 2 to power 53
+// bigint
+// String => " "
+// Null => Stand alone value
+// Undefined => not assigned any value
+//  Symbol => Unique 
+
+//  Obejct
+
+console.log(typeof "Asim")
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+

53c739fa07f8a170d01b4c9e0bf599579bfbc634
WebDevAsimAli <146151821+WebDevAsimAli@users.noreply.github.com>
2023-10-19 06:43:16 +0000

    switch

---
+++ 04_Control Flow/Switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Fabruary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+         break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default month is December");
+        break;
+}
